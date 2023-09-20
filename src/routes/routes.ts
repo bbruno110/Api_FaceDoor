077faa67263625541a0880989421679919038a27
bbruno110 <bruno.os230@gmail.com>
2023-09-20 09:02:14 -0300

    criacao rota login

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,5 +4,6 @@ import * as home from "../controllers/Cadastro";
 const router = Router();
 
 router.route('/register').post(home.Cadastro);
+router.route('/login').post(home.Cadastro);
 
 export default router;
